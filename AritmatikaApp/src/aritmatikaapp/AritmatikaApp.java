62b736670defee46fcf01b58df6231a12e681f76
stevania <stevania.dessya@mhs.itenas.ac.id>
2026-01-07 09:40:00 +0700

    hallo

---
+++ AritmatikaApp/src/aritmatikaapp/AritmatikaApp.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aritmatikaapp;
+
+/**
+ *
+ * @author Stevania Dessya
+ */
+public class AritmatikaApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ AritmatikaApp/src/aritmatikaapp/Operation.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aritmatikaapp;
+
+/**
+ *
+ * @author Stevania Dessya
+ */
+public class Operation {
+    
+}
